8fc6c9770fa61279fae375b97b44b720559bf45f
Simon Wierzchowski <simon.w@selcouth.digital>
2019-03-05 16:39:32 +0000

    chat styling added

---
+++ ios/ChatStyling.h
@@ -0,0 +1,27 @@
+/*	
+ *	
+ *  ChatStyling.h	
+ *  ZDCChat	
+ *	
+ *  Created by Zendesk on 03/12/2014.	
+ *	
+ *  Copyright (c) 2016 Zendesk. All rights reserved.	
+ *	
+ *  By downloading or using the Zendesk Mobile SDK, You agree to the Zendesk Master	
+ *  Subscription Agreement https://www.zendesk.com/company/customers-partners/#master-subscription-agreement and Application Developer and API License	
+ *  Agreement https://www.zendesk.com/company/customers-partners/#application-developer-api-license-agreement and	
+ *  acknowledge that such terms govern Your use of and access to the Mobile SDK.	
+ *	
+ */
+
+#import <Foundation/Foundation.h>
+#import <ZDCChat/ZDCChat.h>
+
+@interface ChatStyling : NSObject
+
+/**	
+ * Apply the chat styling.	
+ */
++ (void)applyStyling;
+
+@end
